d952b1b42ad82d40b0eb741f189d2525b73e2d19
Harrison <harrison@theshoebridges.com>
2015-03-19 10:39:23 +1100

    setup: add basic configuration

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='distance_matrix',
+      author='Harrison Shoebridge',
+      author_email='harrison@theshoebridges.com',
+      description='A small wrapper around the google distance matrix api',
+      version='0.1',
+      py_modules=['distance_matrix'])
