94b2062202dca7f00b1d96f4a939d92ed60e7838
Harrison <harrison@theshoebridges.com>
2015-03-19 10:25:13 +1100

    main: move function into object

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import requests
 
-TRANSIT = "transit"
-DRIVING = "driving"
+class DistanceMatrix(object): 
+    TRANSIT = "transit"
+    DRIVING = "driving"
 
-TRANSIT_BUS = "bus"
-TRANSIT_TRAIN = "train"
+    TRANSIT_BUS = "bus"
+    TRANSIT_TRAIN = "train"
 
-def distance(origin, destination, key, mode=TRANSIT, transit_mode=TRANSIT_TRAIN):
-    payload = {'origins': origin,
-               'destinations': destination,
-               'key': key,
-               'mode': mode,
-               'transit_mode': transit_mode}
+    def __init__(self, key):
+        self.key = key 
 
-    r = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json',
-                     params=payload)
+    def distance(self, origin, destination, mode=TRANSIT, transit_mode=TRANSIT_TRAIN):
+        payload = {'origins': origin,
+                   'destinations': destination,
+                   'key': self.key,
+                   'mode': mode,
+                   'transit_mode': transit_mode}
 
-    data = r.json()
-    
-    distance_value = data["rows"][0]["elements"][0]["distance"]["value"]
-    return distance_value
+        r = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json',
+                         params=payload)
+
+        data = r.json()
+        
+        distance_value = data["rows"][0]["elements"][0]["distance"]["value"]
+        return distance_value
 
